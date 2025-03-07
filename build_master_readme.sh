43d34195cbaefc4e1bb83937dc0f350290945643
Neel Agarwal <neelkagarwal@gmail.com>
2025-03-07 16:26:06 -0600

    Improve README file handling in build_master_readme.sh with enhanced logging and updated copy logic

--- build_master_readme.sh
+++ build_master_readme.sh
@@ -10,17 +10,22 @@ MASTER_README="$README_mgmt/MASTER_README.md"
 # 1) # Go to the folder containing your README files:
 # **********************************************
 cd "$repos_root" || exit 1
+echo -e "\nWorking Dir:\n$PWD\n"
 
-i=2
+# i=2
+echo -e "Looping through existing repositories...\n"
 for repo in */; do
     if [ -d "$repo" ]; then
         cd "$repo" || exit 1
-        if [ "$i" -lt 10 ]; then
-            file_name=0"$i""${repo//"-challenge"/""}"_README.md
-        fi
-        cp README.md "$READMEs_dir"/"$file_name"
+        echo -e "Grabbing README.md from $repo\n"
+        # if [ "$i" -lt 10 ]; then
+        #     file_name=0"$i""${repo//"-challenge"/""}"_README.md
+        # else
+        #     file_name="$i""${repo//"-challenge"/""}"_README.md
+        # fi
+        cp ./README.md ./"$READMEs_dir/$repo-README.md"
     fi
-    i=$((i+1))
+    # i=$((i+1))
 done
 
 # **********************************************
@@ -47,13 +52,10 @@ do
     echo "Adding $file..."
     {
         cat "$file"
-        echo ""  # Add a blank line separator
-        echo "#EOF" # Add an EOF separator too
-        echo ""
+        echo -e "\n#EOF\n" # Add an EOF separator
     } >> ../MASTER_README.md
 done
 
 cd ~ || exit 1
 
-echo ""
-echo "MASTER_README.md has been created/updated."
+echo -e "\nMASTER_README.md has been created/updated."
